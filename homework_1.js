240283cfde3409962e37bc2d3bfe39445ac6b36f
Vitalii Ismagilov <ismagilov.vel@gmail.com>
2021-09-06 11:42:52 +0300

    JS basics | Lesson 1 | Variables, data types, operators

---
+++ homework_1.js
@@ -0,0 +1,240 @@
+//LESSON 1: HOMEWORK
+
+
+//1. Create the variable with the name => first and declare the value 'Hello'
+//2. Create the variable with the name => second and declare the value 'World!'
+//3. Create the variable with the name => greeting and declare the value of the variable first and second
+//4. Print the result of greeting to console
+//the result in the end should be "Hello World!"
+
+//1st version:
+// let first = 'Hello';
+// let second = 'World!';
+// let greeting = `${first} ${second}`;
+// console.log(greeting);
+
+//2nd version:
+// let first1 = 'Hello';
+// let second1 = 'World!';
+// let greeting1 = first1 + " " + second1;
+// console.log(greeting1);
+
+//3d version:
+// let first2 = 'Hello ';
+// let second2 = 'World!';
+// let greeting2 = first1 + " " + second1;
+// console.log(greeting1);
+
+      //===================================
+
+
+
+//1. Create the variable with the name => numberOne, and declare the value to 10, Print the result to console
+
+//2. Create the variable with the name => numberTwo, and declare the value to 12, Print the result to console.
+
+//3. Create the variable with the name => sum(сумма), and declare the value of numberOne plus numberTwo, Print the result to console.
+
+//4. Create the variable with the name => subtract(отнять), and declare the result of  numberOne minus numberTwo (and reverse numberTwo minus numberOne), Print both results to console.
+
+//5.Do the same with multiplication and division.
+
+// let numberOne = 10;
+// console.log(numberOne);
+
+// let numberTwo = 12;
+// console.log(numberTwo);
+
+// let sum = numberOne + numberTwo
+// console.log(sum);
+
+// let subtract = numberOne - numberTwo
+// console.log(subtract);
+
+// let multiplication = numberOne * numberTwo;
+// console.log(multiplication);
+
+// let division = numberOne / numberTwo;
+// console.log(division);
+
+// let remainderOfTheDivision = numberTwo % numberOne;
+// console.log(remainderOfTheDivision);
+  
+      //===================================
+
+
+
+//1. Create the variable with the name => name, declare the value "John", Print the result to console.
+
+//2. Create the variable with the name => age, declare the value 41, Print the result to console.
+
+
+//3. Create the variable with the name gratings, declare the value of the sum of variables name & age, Print the result to console.
+
+// let name = 'John';
+// console.log(name);
+
+// let age = 41;
+// console.log(age);
+
+// let gratings = name + " " + age;
+// console.log(gratings);
+
+        //===================================
+
+
+
+// 1. Create the variable with the name a, declare the value of true, reesign the value to false. Print the result to console.
+
+// let a = true;
+// a = false;
+// console.log(a);
+
+        //===================================
+
+
+
+// 1. Создайте переменные с именами a, b и с, присвойте переменным любые цифровый значения (целые или дробные).  
+// С помощью переприсваивания переменных увеличьте a на 2, уменьшите b на 4 и разделите с на 6, умножьте d на 5. 
+// Выведите a, b, c, d в консоль.
+
+// let a = 4;
+// let b = 6;
+// let c = 12.5;
+// let d = 3;
+
+// a += 2; // or a = a + 2;
+// b -= 4; // or b = b -4;
+// c /= 6; // or c = c / 6;
+// d *= 5; // or d = d * 5;
+
+// console.log(a, b, c, d);
+
+      //===================================
+
+
+
+//fixe the code
+//1.Print the result of the sum
+
+// let a = 1,
+// lat b = 2,
+// const result = 0
+// result = a + b;
+// console.log('result');
+
+//Fixed version:
+// let a = 1;
+// let b = 2;
+// let result;
+// result = a + b;
+// console.log(result);
+
+//2.Print the result of string concatination
+// let 1name = Hello;
+// let 2name = 'World':
+// let str = 1name + name;
+// console.log(str);
+
+//Fixed version:
+// let name1 = 'Hello';
+// let name2 = 'World';
+// let str = name1 + " " + name2;
+// console.log(str);
+
+//3.Print the result of multiplication 
+// let a = 4;
+// let b 2;
+// let multiple = a x b;
+// console.log(multiple);
+
+//Fixed version:
+// let a = 4;
+// let b = 2;
+// let multiple = a * b;
+// console.log(multiple);
+
+      //===================================
+
+
+
+//1. Find the perimetr and area of the square. Console the result.
+//let squareSide = 10;
+
+// let squareSide = 10;
+// let perimetr = squareSide * 4;
+// console.log('Perimetr of the square is ' + perimetr + ' m');
+
+// let area = squareSide * 2;
+// console.log('Area of the square is ' + area + ' m2');
+
+      //===================================
+
+
+
+//2. Find the perimetr and area of the rectangle. Console the result.
+// let a = 10;
+// let b = 5;
+
+// let a = 10;
+// let b = 5;
+
+// let perimetr = (a * 2 ) + (b * 2);
+// console.log('Perimetr of the rectangle is ' + perimetr + ' m');
+
+// let area = a * b;
+// console.log('Area of the rectangle is ' + area + ' m2');
+
+      //===================================
+
+
+
+//1. Crate a variable with name - price and assign the value 5; 
+//2. Create a variable with name - amount and assign the value 10;
+//3. Create a variable with name totalPrice and Calculate the results;
+//4. Console log string 'The total price is {put total price here} for {put amount here} items'
+
+// let price = 5;
+// let amount = 10;
+// let totalPrice = price * amount;
+// console.log(`The total price is ${totalPrice + "$"} for ${amount} items`);
+
+      //===================================
+
+
+
+//1.
+//let a = 10;
+//let b = 5;
+
+//change the value to a = 5, b = 10;
+
+// let a = 10;
+// let b = 5;
+
+// a -=5; // or a = a - 5;
+// b +=5; // or b = b + 5;
+// console.log(a, b);
+
+// Here is another (more smarter) option to solve this using js trick. 
+// Knowing how to destructure an array, it’s easy to use it for swapping variables. 
+// Let’s swap the variables a and b using destructuring assignment:
+
+// let a = 10;
+// let b = 5;
+
+// [a, b] = [b, a];
+// console.log(a, b);
+
+      //===================================
+
+
+
+//codewars.com:
+
+
+//https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/javascript
+
+//https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+//https://www.codewars.com/kata/5859d3325a8a0747180000c6/train/javascript
